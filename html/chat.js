1b6bd88ab4828a540a10279259c39b7e5e484a83
Joel Ward <jmward@gmail.com>
2015-09-11 21:47:55 -0700

    tigthen up code a little

--- html/chat.js
+++ html/chat.js
@@ -1,17 +1,16 @@
 function Application(cfg){this.cfg=cfg}
 Application.prototype.start = function() {
-    var cfg = this.cfg;
-    var ws = this.ws = new WebSocket('ws://'+cfg.host+'/ws');
-    ws.onopen=function(){
+    this.ws = new WebSocket('ws://'+this.cfg.host+'/ws');
+    this.ws.onopen=function(){
 	    console.log("on open",this);
     }.bind(this);
-    ws.onerror=function(){
+    this.ws.onerror=function(){
 	    console.log("on error");
     }.bind(this);
-    ws.onclose=function(){
+    this.ws.onclose=function(){
 	    console.log("on close");
     }.bind(this);
-    ws.onmessage=function(evt){
+    this.ws.onmessage=function(evt){
 	    console.log("on message",evt.data);
       var msg = JSON.parse(evt.data);
       var typ = msg[0];
